52e11da63fc78c53f22af6b5b3b25e152497feb6
Jeremy <jeremy@likefury.com>
2022-12-04 20:59:08 +1300

    added data schema and initial readme

---
+++ database/migrations/2022_12_04_074616_create_purchases_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('purchases', function (Blueprint $table) {
+            $table->id();
+            $table->integer('quantity');
+            // Laravel default is 2 decimal places which will be ideal for a price
+            $table->decimal('price');
+            // By adding a consumed column here and keeping it up to date, we can create performant lookups rather than recalculating applications or performing application lookups
+            $table->integer('consumed');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('purchases');
+    }
+};
