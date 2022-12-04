2e4cb4fc45f4a7a5d7989b4b1d23d30e35d46486
Jeremy <jeremy@likefury.com>
2022-12-04 22:28:04 +1300

    refactor: inventory application advanced

--- app/Domain/Inventory/Actions/InventoryApplicationAction.php
+++ app/Domain/Inventory/Actions/InventoryApplicationAction.php
@@ -7,11 +7,29 @@ use App\Models\Purchase;
 
 final class InventoryApplicationAction
 {
+    /**
+     * Create an application, its relationship to purchase and update the purchase consumed columns
+     * @param int $requestQuantity
+     * @return Application
+     */
     public function execute(int $requestQuantity): Application
     {
         $application = new Application();
         $application->save();
 
+        $this->processPurchases($application, $requestQuantity);
+
+        return $application;
+    }
+
+    /**
+     * Loop through valid purchases and calculate how many we will consume of each
+     * @param Application $application
+     * @param int $requestQuantity
+     * @return void
+     */
+    private function processPurchases(Application $application, int $requestQuantity)
+    {
         $purchases = Purchase::where('consumed', '<', 'quantity')->get();
 
         foreach ($purchases as $purchase) {
@@ -19,21 +37,44 @@ final class InventoryApplicationAction
             // Exit the loop if there is no more to do, saves on DB queries
             if ($requestQuantity == 0) continue;
 
-            if ($requestQuantity > $purchase->quantity) {
-                $consumedQuantity = $requestQuantity - $purchase->quantity;
-            } else {
-                $consumedQuantity = $purchase->quantity - $requestQuantity;
-            }
+            $consumedQuantity = $this->calculateConsumedQuantity($purchase, $requestQuantity);
 
             $requestQuantity -= $consumedQuantity;
-            $purchase->consumed = $consumedQuantity;
-            $purchase->save();
 
-            $application->purchases()->attach($purchase, [
-                'quantity' => $consumedQuantity
-            ]);
+            $this->savePurchaseAndEstablishRelationship($application, $purchase, $consumedQuantity);
+        }
+    }
+
+    /**
+     * Calculate the purchase consumed amount off the given request quantity
+     * @param Purchase $purchase
+     * @param int $requestQuantity
+     * @return int
+     */
+    private function calculateConsumedQuantity(Purchase $purchase, int $requestQuantity): int
+    {
+        if ($requestQuantity > $purchase->quantity) {
+            $consumedQuantity = $requestQuantity - $purchase->quantity;
+        } else {
+            $consumedQuantity = $purchase->quantity - $requestQuantity;
         }
 
-        return $application;
+        return $consumedQuantity;
+    }
+
+    /**
+     * Establish the relationship between the application and purchase with the quantity
+     * @param Application $application
+     * @param Purchase $purchase
+     * @return void
+     */
+    private function savePurchaseAndEstablishRelationship(Application $application, Purchase $purchase, int $consumedQuantity): void
+    {
+        $purchase->consumed = $consumedQuantity;
+        $purchase->save();
+
+        $application->purchases()->attach($purchase, [
+            'quantity' => $consumedQuantity
+        ]);
     }
 }
